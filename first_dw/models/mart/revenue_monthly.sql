51e7731ae857524f6cd3a1d5a08612dc0fb05681
ayyoubmaul <underestimate70@gmail.com>
2024-03-07 21:16:46 +0700

    update schema

--- first_dw/models/mart/revenue_monthly.sql
+++ first_dw/models/mart/revenue_monthly.sql
@@ -5,8 +5,10 @@ select
 	SUM(quantity * revenue) as total_revenue
     , dt.month
     , dt.year
+    , dp.product_name
 from {{ ref('fact_sales') }} as fs
 left join {{ ref('dim_time') }} as dt on fs.date_id = dt.date_id
-group by dt.month, dt.year
+left join {{ ref('dim_product') }} as dp on dp.product_id = fs.product_id
+group by dt.month, dt.year, dp.product_name
 
 -- where dt.month = 'Jan' and dt.year = 2024;
